8f4b77aeedfa065417c1ff5a7bbafeaddd2ce157
Bogdan <58550245+Pinkavsky@users.noreply.github.com>
2020-04-27 19:33:15 +0300

    oper_systems_lab3

---
+++ system_emulation/stdout.py
@@ -0,0 +1,36 @@
+from scipy.stats import poisson
+from scipy import mean
+
+
+class StandardOutput:
+    def __init__(self):
+        self._counter = 0
+        self._history = []
+
+    def smooth(self):
+        if len(self._history) > 0:
+            self._history.sort(key=lambda bid:
+                               bid.processing_start - bid.advent)
+
+            mu = mean([bid.processing_start - bid.advent
+                       for bid in self._history])
+
+            p = poisson.rvs(mu, size=len(self._history))
+            p.sort()
+
+            for i in range(len(self._history)):
+                self._history[i]._start = self._history[i].advent + p[i]
+
+            self._history.sort(key=lambda bid: bid._end)
+
+    def echo(self, task):
+        self._counter += 1
+        # print('Task #', self._counter,
+        #       ' advent:', task.advent, '|',
+        #       'complexity:', task.complexity, '|',
+        #       'processing start:', task.processing_start, '|',
+        #       'end:', task.processing_end)
+        self._history.append(task)
+
+    def history(self):
+        return self._history
